415cdc983c6f7da41d042f9432c05befbea3505a
alis <asyed5839@gmail.com>
2021-06-12 12:57:12 +0100

    Functionality for taking user input
    Two onChange listeners and functions have been inserted for taking user data via states. Also "set" method have been added for clearing the form once the form has been submitted.

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -1,18 +1,30 @@
 import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
 import Card from "../UI/Card";
+import { useState } from "react";
 function AddUser() {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
     const addUserHandler = (event) => {
-        event.preventDefault();
+      event.preventDefault();
+      setEnteredUsername('');
+      setEnteredAge('');
     }; 
+    const usernameChangeHandler = (event) => {
+      setEnteredUsername(event.target.value);
+    };
+    const ageChangeHandler = (event) => {
+      setEnteredAge(event.target.value);
+    };
 
   return (
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
-          <input id="username" type="text" />
+          <input id="username" type="text" onChange={usernameChangeHandler}/>
           <label htmlFor="userage">Age</label>
           <input id="userage" type="number" />
-          <button type="submit">Add USer</button>
+          <Button type="submit" onChange={ageChangeHandler}>Add USer</Button>
         </form>
       </Card>
   );
