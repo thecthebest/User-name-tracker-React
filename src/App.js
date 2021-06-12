f28439b446d0990e2456a5cb42f73cc8dfd249d2
alis <asyed5839@gmail.com>
2021-06-12 21:39:09 +0100

    Added a function and states to move data
    A function to add new data to the previousdata and props to move data
    from AddUser to UserList.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 import AddUser from './components/User/AddUser';
+import UserList from './components/User/UserList';
 
 function App() {
+  const [userList, setUserList] = useState([]);
+  const addUserHandler = (user) => {
+    setUserList((prevstate) => {
+      return [...prevstate, user];
+    });
+  };
   return (
     <div>
-      <AddUser></AddUser>
+      <AddUser onAddUser={addUserHandler}/>
+      <UserList users={userList}/>
     </div>
   );
 }
