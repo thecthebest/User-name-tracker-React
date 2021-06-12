99fc207c278ca7af614facadcbe6ef70968c422f
alis <asyed5839@gmail.com>
2021-06-12 09:14:21 +0100

    Added Card function and styles
    Files affected:
    Card.js: Card function was added
    Card.module.css: Card class was added

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,1 +1,11 @@
+import classes from "./Card.module.css";
+function Card(props) {
 
+  return (
+      <div className={classes.Card}>
+          {props.children}
+      </div>
+  );
+}
+
+export default Card;
