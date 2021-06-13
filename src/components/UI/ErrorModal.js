99dd36fdd392cd3370838a5d81eec9c624dc2f1a
alis <asyed5839@gmail.com>
2021-06-13 10:02:14 +0100

    Two divs been added
    Two divs have been inserted, one acts as a wrapper for the jsx code
    and another is used to  provide overlay and prevent user from clicking the
    background.

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -3,17 +3,20 @@ import Card from "./Card";
 import Button from "./Button";
 function ErrorModal(props) {
     return (
-        <Card className={classes.modal}>
-            <header className={classes.header}>
-                <h2>{props.title}</h2>
-            </header>
-            <div className={classes.content}>
-                <p>{props.message}</p>
-            </div>
-            <footer className={classes.actions}>
-                <Button>Okay</Button>
-            </footer>
-        </Card>
+        <div>
+            <div className={classes.backdrop}></div>
+            <Card className={classes.modal}>
+                <header className={classes.header}>
+                    <h2>{props.title}</h2>
+                </header>
+                <div className={classes.content}>
+                    <p>{props.message}</p>
+                </div>
+                <footer className={classes.actions}>
+                    <Button>Okay</Button>
+                </footer>
+            </Card>
+        </div>
     );
 }
 
