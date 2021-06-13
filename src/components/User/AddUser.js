ae5948c206ba37a09f9ff6d8414f27b8d79f8c3c
alis <asyed5839@gmail.com>
2021-06-13 15:48:47 +0100

    Added a new usestate error and error object
    AddUser.js: Added a new usesate error to store different values of error
    objects that is created according to the user validation. Depending on
    the error detected one error object is cretaed in the "if"
    statements. In addition, if the error object is not defined this
    prevents ErrorModal from showing.

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -6,12 +6,21 @@ import { useState } from "react";
 function AddUser(props) {
   const [enteredUsername, setEnteredUsername] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
+  const [error, setError] = useState();
     const addUserHandler = (event) => {
       event.preventDefault();
       if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+        setError({
+          title: "Invalid input",
+          message: "Please enter a valid name and age (non-empty values)."
+        });
         return;
       }
       if (+enteredAge < 1) {
+        setError({
+          title: "Invalid age",
+          message: "Please enter a valid name and age (non-empty values)."
+        });
         return;
       }
       const UserData = {
@@ -32,7 +41,7 @@ function AddUser(props) {
 
   return (
     <div>
-      <ErrorModal title="An error occured!" message="Something went wrong" />
+      {error && <ErrorModal title={error.title} message={error.message} />}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
