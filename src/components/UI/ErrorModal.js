9bb99eda904d8e446e4b68b7c60f69a264b46b79
alis <asyed5839@gmail.com>
2021-06-13 09:46:43 +0100

    Added a new ErrorModal component
    The new component will be used as an overlay custom component.

---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,20 @@
+import classes from "./ErrorModal.module.css";
+import Card from "./Card";
+import Button from "./Button";
+function ErrorModal(props) {
+    return (
+        <Card className={classes.modal}>
+            <header className={classes.header}>
+                <h2>{props.title}</h2>
+            </header>
+            <div className={classes.content}>
+                <p>{props.message}</p>
+            </div>
+            <footer className={classes.actions}>
+                <Button>Okay</Button>
+            </footer>
+        </Card>
+    );
+}
+
+export default ErrorModal;
