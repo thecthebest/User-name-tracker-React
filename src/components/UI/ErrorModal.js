85c2866eba188d2e8c77f8050143adc2e8603859
thecthebest <asyed5839@gmail.com>
2022-07-03 12:13:42 +0100

    Restructured code to move modal and overlay up
    Modal and overlay component restructured to provide better semantic menaning. This has been achieved by adding seperate html elements in the html file.

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -2,24 +2,46 @@ import classes from "./ErrorModal.module.css";
 import Card from "./Card";
 import Button from "./Button";
 import { Fragment } from "react";
+import ReactDom from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClick}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onClick}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
 // Render modal
 function ErrorModal(props) {
-    return (
-        <Fragment>
-            <div className={classes.backdrop} onClick={props.onConfirm}></div>
-            <Card className={classes.modal}>
-                <header className={classes.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={classes.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={classes.actions}>
-                    <Button onClick={props.onConfirm}>Okay</Button>
-                </footer>
-            </Card>
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      {ReactDom.createPortal(
+        <Backdrop onClick={props.onConfirm}></Backdrop>,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDom.createPortal(
+        <ModalOverlay
+          onClick={props.onConfirm}
+          title={props.title}
+          message={props.message}
+        ></ModalOverlay>,
+        document.getElementById("overlay-root")
+      )}
+    </Fragment>
+  );
 }
 
 export default ErrorModal;
